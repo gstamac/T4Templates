3bac139b419ac457c6465ad28757e64c5d91ebf9
gstamac <G_Stamac@enerdat.com>
2013-09-06 15:44:31 +0200

    ImplementPropertyChangedCaliburn: dont inherit PropertyChangedBase if any parent already inherits it

---
+++ ImplementPropertyChangedCaliburn/test/ClassLibrary1/Class2Parent.Generated.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using Caliburn.Micro;
+
+namespace ClassLibrary1
+{
+	partial class Class2Parent: PropertyChangedBase
+	{
+		private System.String _ParentProperty;
+		public System.String ParentProperty
+		{
+			get { return _ParentProperty; }
+			set { ChangeProperty<System.String>("ParentProperty", ref _ParentProperty, value); }
+		}
+
+
+		#region INotifyPropertyChanged implementation
+
+		protected virtual void ChangeProperty<T>(string propertyName, ref T propertyValue, T newValue, Action<T, T> changedCallback = null)
+		{
+			if ((propertyValue == null && newValue == null) || (propertyValue != null && propertyValue.Equals(newValue))) return;
+			T oldValue = propertyValue;
+			propertyValue = newValue;
+			NotifyOfPropertyChange(propertyName);
+			if (changedCallback != null) changedCallback(oldValue, newValue);
+		}
+
+		public string GetPropertyName<TProperty>(Expression<Func<ClassLibrary1.Class2Parent, TProperty>> propertySelector)
+		{
+			return propertySelector.GetMemberInfo().Name;
+		}
+
+		#endregion
+	}
+
+	partial class Class2Child
+	{
+		private System.String _ChildProperty;
+		public System.String ChildProperty
+		{
+			get { return _ChildProperty; }
+			set { ChangeProperty<System.String>("ChildProperty", ref _ChildProperty, value); }
+		}
+
+	}
+
+	partial class Class2ChildsChild
+	{
+		private System.String _ChildsChildProperty;
+		public System.String ChildsChildProperty
+		{
+			get { return _ChildsChildProperty; }
+			set { ChangeProperty<System.String>("ChildsChildProperty", ref _ChildsChildProperty, value); }
+		}
+
+	}
+}
---
+++ ImplementPropertyChangedCaliburn/test/ClassLibrary1/Class2Parent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplateAttributes;
+
+namespace ClassLibrary1
+{
+    public interface IClass2Parent
+    {
+        string ParentProperty { get; set; }
+    }
+
+    public interface IClass2Child
+    {
+        string ChildProperty { get; set; }
+    }
+
+    public interface IClass2ChildsChild
+    {
+        string ChildsChildProperty { get; set; }
+    }
+
+    [ImplementPropertyChangedCaliburn(typeof(IClass2Parent))]
+    public partial class Class2Parent : IClass2Parent
+    {
+    }
+
+    [ImplementPropertyChangedCaliburn(typeof(IClass2Child))]
+    public partial class Class2Child : Class2Parent, IClass2Child
+    {
+    }
+
+    [ImplementPropertyChangedCaliburn(typeof(IClass2ChildsChild))]
+    public partial class Class2ChildsChild : Class2Child, IClass2ChildsChild
+    {
+    }
+}
